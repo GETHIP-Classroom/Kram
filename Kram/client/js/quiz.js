73c719f7b827a3c3e556b06ff773d52bfad03f28
Cooper Knaak <cooperknaak@gmail.com>
2018-03-01 20:51:17 -0600

    Added next button to quiz. Correct answer now displays on next button.

--- Kram/client/js/quiz.js
+++ Kram/client/js/quiz.js
@@ -2,10 +2,36 @@ import { Questions } from "../../collections/questions.js";
 
 Template.quiz.onCreated(function(){
   Meteor.subscribe("Questions");
+  Template.instance().onAnswer = new ReactiveVar(false);
+  Template.instance().questionIndex = new ReactiveVar(0);
 })
 
 Template.quiz.helpers({
   "currentQuestion": function(){
-    return Questions.findOne();
+      let index = Template.instance().questionIndex.get();
+      let questions = Questions.find().fetch();
+      if (questions.length <= index) {
+          return;
+      }
+    let question = questions[index];
+    for (let i = 0; i < question.answers.length; i++) {
+        let answer = question.answers[i];
+        question.answers[i] = {
+            answer: answer,
+            color: ""
+        };
+        if (Template.instance().onAnswer.get()) {
+            let color = (answer == question.correctAnswer) ? "correctAnswer" : "incorrectAnswer";
+            question.answers[i].color = color;
+        }
+    }
+    console.log(question);
+    return question;
   }
+})
+
+Template.quiz.events({
+    'click #nextButton':function() {
+        Template.instance().onAnswer.set(!Template.instance().onAnswer.get());
+    }
 })
